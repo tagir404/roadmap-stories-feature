eacbcb4b96b9938d978e603205fb5b88dae2660d
Tagir <sulaev.tagir@mail.ru>
2025-06-02 16:53:26 +0300

    vite config upd

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/roadmap-stories-feature/',
   plugins: [
     vue({
       template: {
